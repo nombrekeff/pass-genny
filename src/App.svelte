f82dda960c694e9aba3439c17e3d44b2a81665f0
nombrekeff <manoloedge96@gmail.com>
2021-07-31 13:08:16 +0200

    feat: initial layout in place

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,27 @@
 <script>
-	export let name;
+	import { Router, Route } from "svelte-routing";
+	import Home from "./routes/Home.svelte";
+	import About from "./routes/About.svelte";
+	import Challenge from "./routes/Challenge.svelte";
+	import Sidemenu from './components/Sidemenu.svelte';
+
+	export let url = "";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	.page {
+		display: flex;
+		height: 100%;
 	}
 </style>
+  
+<Router url="{url}">
+	<div class="page">
+		<Sidemenu/>
+		<div class="content-box bg-blue">
+			<Route path="/"><Home /></Route>
+			<Route path="challenge" component="{Challenge}" />
+			<Route path="about" component="{About}" />
+		</div>
+	</div>
+</Router>
---
+++ src/components/Sidemenu.svelte
@@ -0,0 +1,42 @@
+<script>
+    import { Link } from "svelte-routing";
+    import Logo from './Logo.svelte';
+</script>
+
+<style>
+    .sidenav {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-between;
+        background-color: white;
+        width: 15%;
+        min-width: 15%;
+        border-radius: 25px;
+        padding: 80px 0;
+    }
+
+    .sidenav nav {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .version {
+        font-size: 12px;
+        color: var(--col-primary);
+        opacity: 0.6;
+    }
+</style>
+
+<div class="sidenav">
+    <Logo/>
+    <nav>
+        <Link to="/">Home</Link>
+        <Link to="about">About</Link>
+        <Link to="challenge">Challenge</Link>
+        <a href="https://github.com/nombrekef" target="_blank">GitHub</a>
+    </nav>
+
+    <span class="version">v0.0.1</span>
+</div>
